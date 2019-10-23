248303000535c806cb8c5711429974bb407a359f
book-lift <bookliftapp@gmail.com>
2019-10-23 11:11:09 -0700

    Make donation keypad do stuff

--- src/app/donor/donation-view/donation-view.component.ts
+++ src/app/donor/donation-view/donation-view.component.ts
@@ -3,17 +3,36 @@ import { Component, OnInit } from '@angular/core';
 import { NumericInput } from '@angular/numeric-keyboard'
 
 
+const INPUT_PLACEHOLDER_TEXT = "How many books would you like to donate?";
+
 @Component({
   selector: 'app-donation-view',
   templateUrl: './donation-view.component.html',
   styleUrls: ['./donation-view.component.css']
 })
 export class DonationViewComponent implements OnInit {
-
+  displayText = INPUT_PLACEHOLDER_TEXT;
+  showingPlaceholder = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClearPressed() {
+    this.showingPlaceholder = true;
+    this.displayText = INPUT_PLACEHOLDER_TEXT;
+  }
+
+  onKeypadClick(event: any) {
+    const value = event.target.dataset.value;
+    
+    if (this.showingPlaceholder) {
+      this.showingPlaceholder = false;
+      this.displayText = "";
+    }
+
+    this.displayText += value;
   }
   
 }
